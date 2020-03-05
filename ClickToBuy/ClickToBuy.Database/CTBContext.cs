77d541bbabb88344a823c28b24072255d7da7ec7
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-03-05 16:46:58 +0600

    ClickToBuy
    1. Create ClickToBuy.Database class library project.
    2. Create CTBContext class and write the code for connection string.
    3. Install packages for ASP.NET CORE and Entity.
    4. Add migration and update database

---
+++ ClickToBuy/ClickToBuy.Database/CTBContext.cs
@@ -0,0 +1,28 @@
+﻿using ClickToBuy.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickToBuy.Database
+{
+    public class CTBContext : DbContext 
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasIndex(c=>c.Name).IsUnique();
+            modelBuilder.Entity<City>().HasIndex(c=>c.Name).IsUnique();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = DESKTOP-HHA0TOP; 
+                                    Initial Catalog = ClickToBuy; 
+                                    Integrated Security = SSPI;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ ClickToBuy/ClickToBuy.Database/Migrations/20200305103922_AddModel_Country_City.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClickToBuy.Database.Migrations
+{
+    public partial class AddModel_Country_City : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name",
+                table: "Cities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
