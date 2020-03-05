413d169263d8958bbb77854c5ac5923956fd5052
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-03-05 16:14:12 +0600

    ClickToBuy
    1. Create ClickToBuy.Model project.
    2. Create Country model.
    3. Create City model.

---
+++ ClickToBuy/ClickToBuy.Model/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClickToBuy.Model
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required City name.")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Select Country name.")]
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
---
+++ ClickToBuy/ClickToBuy.Model/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClickToBuy.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+    }
+}
