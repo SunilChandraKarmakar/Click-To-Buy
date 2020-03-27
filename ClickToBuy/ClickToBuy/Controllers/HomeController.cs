0fad97a0089c8c0e3183cd482d9c556f7c03b3f0
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-03-27 22:11:01 +0600

    Click To Buy
    1. Load all brand in clinte site.
    2. Load all Category with Sub category in clinte site.

--- ClickToBuy/ClickToBuy/Controllers/HomeController.cs
+++ ClickToBuy/ClickToBuy/Controllers/HomeController.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ClickToBuy.Models;
+using ClickToBuy.Manager.Contracts;
+using ClickToBuy.Model;
 
 namespace ClickToBuy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBrandManager _iBrandManager;
+        private readonly ICategoryManager _iCategoryManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBrandManager iBrandManager, ICategoryManager iCategoryManager)
         {
             _logger = logger;
+            _iBrandManager = iBrandManager;
+            _iCategoryManager = iCategoryManager;
         }
 
         public IActionResult Index()
         {
+            ViewBag.BrandList = _iBrandManager.GetAll();
+            ViewBag.CategoryList = _iCategoryManager.GetAll().Where(c => c.Categoryy == null).ToList();
             return View();
         }
 
