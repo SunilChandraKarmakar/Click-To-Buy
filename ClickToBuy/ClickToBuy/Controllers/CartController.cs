679bd1a995a5943e0fd77db87eeee2fbd14207a0
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-06-08 11:58:23 +0600

    Use Session
    1, Create Utility folder in ClickToBuy project and add a static class SessionExtensions.
    2. Working with StartUp class.

--- ClickToBuy/ClickToBuy/Controllers/CartController.cs
+++ ClickToBuy/ClickToBuy/Controllers/CartController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClickToBuy.Model;
+using ClickToBuy.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,7 +15,12 @@ namespace ClickToBuy.Controllers
         {
             if(HttpContext.Session.GetString("CustomerId") != null)
             {
+                List<Product> addProducts = HttpContext.Session.Get<List<Product>>("AddProducts");
 
+                if (addProducts == null)
+                    addProducts = new List<Product>();
+
+                return View(addProducts);
             }
 
             return RedirectToAction("CustomerLogin", "LoginProcess");
--- ClickToBuy/ClickToBuy/Startup.cs
+++ ClickToBuy/ClickToBuy/Startup.cs
@@ -34,6 +34,15 @@ namespace ClickToBuy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -78,14 +87,6 @@ namespace ClickToBuy
             services.AddTransient<IAdminRepository, AdminRepository>();
             services.AddTransient<ISliderRepository, SliderRepository>();
             services.AddTransient<ISliderManager, SliderManager>();
-
-            services.AddDistributedMemoryCache();
-            services.AddSession();
-
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(10);
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
